ad6ae569fdaa87696a0f66f32ba6a64c7cd9d1b0
mateusmanetta <mateusmanetta8@gmail.com>
2018-08-19 14:50:32 -0300

    Initial commit

---
+++ app/src/main/java/br/com/mateus/sugarme/MainActivity.java
@@ -0,0 +1,34 @@
+package br.com.mateus.sugarme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() == null) {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build());
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    1);
+        } else {
+
+        }
+    }
+}
